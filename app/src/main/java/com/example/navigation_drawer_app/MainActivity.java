75b176ec83311c0e35c2b92b9ce23c6b6d3ce96e
elrosell <roydandc@gmail.com>
2025-11-19 20:25:17 -0600

    Add navigation drawer with sample sections

--- app/src/main/java/com/example/navigation_drawer_app/MainActivity.java
+++ app/src/main/java/com/example/navigation_drawer_app/MainActivity.java
@@ -3,22 +3,90 @@ package com.example.navigation_drawer_app;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.top_app_bar);
+
+        ViewCompat.setOnApplyWindowInsetsListener(drawerLayout, (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            Fragment fragment;
+            String title;
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_profile) {
+                fragment = new ProfileFragment();
+                title = getString(R.string.menu_profile);
+            } else if (itemId == R.id.nav_settings) {
+                fragment = new SettingsFragment();
+                title = getString(R.string.menu_settings);
+            } else {
+                fragment = new HomeFragment();
+                title = getString(R.string.menu_home);
+            }
+
+            replaceFragment(fragment, title);
+            drawerLayout.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_home);
+            replaceFragment(new HomeFragment(), getString(R.string.menu_home));
+        }
+    }
+
+    private void replaceFragment(Fragment fragment, String title) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, fragment)
+                .commit();
+        setTitle(title);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
